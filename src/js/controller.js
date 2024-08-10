5b32882c91fe264cef73cc4db37a816386ea13f5
Alex <alexander-replyansky@yandex.ru>
2024-08-10 23:20:27 +0300

    adding markup function which applies data received from api

--- src/js/controller.js
+++ src/js/controller.js
@@ -13,11 +13,102 @@ const timeout = function (s) {
 
 const showRecipe = async function () {
   try {
-    const res = await fetch('https://api.spoonacular.com/recipes/716429/information?apiKey=d754bd859d5c40abaf88e8715002bd21&includeNutrition=false');
+    const res = await fetch('https://api.spoonacular.com/recipes/654959/information?apiKey=d754bd859d5c40abaf88e8715002bd21');
     const data = await res.json();
+
+    if(!res.ok) throw new Error(`${data.message}`)
+
     console.log(data)
+
+    const markup = `
+        <figure class="recipe__fig">
+              <img src="${data.image}" alt="${data.title}" class="recipe__img" />
+              <h1 class="recipe__title">
+                <span>${data.title}</span>
+              </h1>
+        </figure>
+
+        <div class="recipe__details">
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="src/img/icons.svg#icon-clock"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--minutes">${data.readyInMinutes}</span>
+            <span class="recipe__info-text">minutes</span>
+          </div>
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="src/img/icons.svg#icon-users"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--people">${data.servings}</span>
+            <span class="recipe__info-text">servings</span>
+
+            <div class="recipe__info-buttons">
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="src/img/icons.svg#icon-minus-circle"></use>
+                </svg>
+              </button>
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="src/img/icons.svg#icon-plus-circle"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="recipe__user-generated">
+            <svg>
+              <use href="src/img/icons.svg#icon-user"></use>
+            </svg>
+          </div>
+          <button class="btn--round">
+            <svg class="">
+              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+            </svg>
+          </button>
+        </div>
+
+        <div class="recipe__ingredients">
+          <h2 class="heading--2">Recipe ingredients</h2>
+          <ul class="recipe__ingredient-list">
+            ${data.extendedIngredients.map(ing => {
+              return `
+              <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="src/img/icons.svg#icon-check"></use>
+              </svg>
+            <div class="recipe__quantity">${ing.amount}</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">${ing.unit}</span>
+                of ${ing.name}
+              </div>
+            </li>
+            `}).join('')}
+        </div>
+        <div class="recipe__directions">
+          <h2 class="heading--2">How to cook it</h2>
+          <p class="recipe__directions-text">
+            This recipe was carefully designed and tested by
+            <span class="recipe__publisher">${data.creditsText}</span>. Please check out
+            directions at their website.
+          </p>
+          <a
+            class="btn--small recipe__btn"
+            href="${data.sourceUrl}"
+            target="_blank"
+          >
+            <span>Directions</span>
+            <svg class="search__icon">
+              <use href="src/img/icons.svg#icon-arrow-right"></use>
+            </svg>
+          </a>
+        </div>
+    `;
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
-    console.log(err)
+    alert(err)
   }
   
 }
