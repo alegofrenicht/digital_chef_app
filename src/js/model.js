df8816e0c2cd33d6bb9bc7a557b3beaa7c7e6e6b
Alexander Replyansky <alexander9replyansky@gmail.com>
2024-08-19 16:11:22 +0300

    adding config file and refactoring code exporting variables from config

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,5 @@
-import { API_URL } from "./config.js";
+import { API_URL, API_KEY } from "./config.js";
+import { getJSON } from "./helpers.js";
 
 export const state = {
     recipe: {},
@@ -6,7 +7,7 @@ export const state = {
 
 export const loadRecipe = async function(id){
     try{
-    
+        const data = await getJSON(`${API_URL}${id}/${API_KEY}`);
     state.recipe = {
         id: data.id,
         title: data.title,
--- src/js/views/helpers.js
+++
@@ -1,9 +0,0 @@
-export const getJSON = async function (url) {
-    const res = await fetch(url);
-    const data = await res.json();
-    
-    if(!res.ok) throw new Error(`${data.message}`)
-}
-
-
-// `${API_URL}${id}/information?apiKey=d754bd859d5c40abaf88e8715002bd21`
